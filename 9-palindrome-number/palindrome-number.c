eb32bddd89465798d56f8aa49d247d19a7e57a88
hsiao93524 <69866145+hsiao93524@users.noreply.github.com>
2024-05-09 22:28:26 +0900

    Time: 3 ms (92.74%) | Memory: 6 MB (30.08%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,60 @@
+bool by_int(long x){
+    
+    // compare after inverse
+    long temp = x;
+    long after = 0;
+
+    // inverse
+    while(temp != 0){
+        after = after * 10 + temp % 10;
+        temp = temp / 10;
+    }
+
+    if(x == after){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool by_str(long x){
+    // transfer to string
+    int size = (floor(log10(x))+1);
+    char str[size + 1]; // including '/0'
+    sprintf(str, "%d", x);
+
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }    
+    return true;
+}
+
+bool isPalindrome(long x) {
+    
+    if(x < 0){
+        return false;
+    }
+    else if(x == 0){
+        return true;
+    }
+
+    int solution = 2;
+
+    switch(solution){
+        case 1:
+            return by_str(x); // 7-17ms       
+            break;
+        case 2:
+            return by_int(x); // 7ms
+            break;
+        default:
+            return false;
+            break;
+    }
+}
